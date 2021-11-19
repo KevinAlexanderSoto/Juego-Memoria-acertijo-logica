3d1a062df78229ba6091b7ab95a8efea4c275ec2
Kevin Soto <kevinalexandersoto999@gmail.com>
2021-11-18 22:35:29 -0500

    mejoras visuales y final juego 3

--- js/juego3.js
+++ js/juego3.js
@@ -1,11 +1,10 @@
-
 const btnRelog = document.querySelector("#btnReloj");
-
-
+const Mbody = document.querySelector("#Mbody");
 window.addEventListener('DOMContentLoaded',(event)=>{
     // Mostrar el toast
     const toastEl = document.querySelector('#toast');
     const toast = new bootstrap.Toast( toastEl );
+   
     toast.show();
      // guardar las letras para ser vistas desde otra 
      localStorage.setItem('grupo2',' o ? m e i u');
@@ -14,10 +13,16 @@ window.addEventListener('DOMContentLoaded',(event)=>{
    btnRelog.addEventListener('submit',(ev)=>{
     ev.preventDefault();
     const respuesta = document.querySelector("#respuesta");
+
     if(respuesta.value != "11:50"){
+      Mbody.textContent = "Esa no es!!! , intenta otra vez";
        return console.log(' respuesta incorrecta' );
-        
+       
     }
-    localStorage.setItem('grupo3','e i s r a v');
-    console.log(' respuesta correcta!!!!!' );
+      localStorage.setItem('grupo3','e i s r a v');
+      Mbody.textContent = "Has ganado , pasemos a descifrar la frace final. Ya tienes toda las letras";
+
+      setTimeout(()=>{
+         window.location.replace("/palabra.html");
+      },4000);
    }) 
