d69ba7b26bde700db61655ca609b67d532a7b097
Kevin Soto <kevinalexandersoto999@gmail.com>
2021-11-19 18:48:20 -0500

    letras y detalles

--- js/palabra.js
+++ js/palabra.js
@@ -4,17 +4,32 @@ const grupo1 = localStorage.getItem('grupo1');
 const grupo2 = localStorage.getItem('grupo2');
 const grupo3 = localStorage.getItem('grupo3');
 
-console.log(grupo1,grupo2);
+const l1 = document.querySelector("#l1");
+const l2 = document.querySelector("#l2");
+const l3 = document.querySelector("#l3");
+const l4 = document.querySelector("#l4");
 
-if(grupo1 != null){
+const letras = ()=>{
 
-    h2.textContent = '' + grupo1 ;
-};
+    if(grupo1 != null){
 
-if(grupo2!=null){
-h2.textContent = h2.textContent + grupo2;
-};
-
-if(grupo3!=null){
-    h2.textContent = h2.textContent +" "+ grupo3;
+        h2.textContent = '' + grupo1 ;
     };
+    
+    if(grupo2!=null){
+    h2.textContent = h2.textContent + grupo2;
+    };
+    
+    if(grupo3!=null){
+        h2.textContent = h2.textContent +" "+ grupo3;
+        };
+
+}
+
+const validarCampo = (contenido)=>{
+
+
+
+}
+
+letras();
