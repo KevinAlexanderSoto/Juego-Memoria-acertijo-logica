9e8e2aa7a47ca227057319cd2a5cf673b7c55778
Kevin Soto <kevinalexandersoto999@gmail.com>
2021-11-21 13:49:48 -0500

    casi listo

--- js/palabra.js
+++ js/palabra.js
@@ -9,6 +9,7 @@ const l2 = document.querySelector("#l2");
 const l3 = document.querySelector("#l3");
 const l4 = document.querySelector("#l4");
 
+let ganados=0;
 const letras = ()=>{
 
     if(grupo1 != null){
@@ -26,10 +27,65 @@ const letras = ()=>{
 
 }
 
-const validarCampo = (contenido)=>{
+    l1.addEventListener('keyup',(ev)=>{
+       if (l1.value.length == 7) {
+        let cont = 0;
+        if (l1.value == "Quieres" && cont !=1) {
+            l1.className = "input border border-success fs-2 rounded-1"; 
+            ganados++;
+            cont=1;   
+        }
+          
+       }
+    });
 
+    l2.addEventListener('keyup',(ev)=>{
+        ev.preventDefault(); 
+        if (l2.value.length == 3) {
+            let cont = 0;
+         if (l2.value == "ser" && cont !=1) {
+             l2.className = "input border border-success fs-2 rounded-1"    
+             ganados++;
+             cont=1; 
+            }
+           
+        }
+     });
 
+     l3.addEventListener('keyup',(ev)=>{
+        ev.preventDefault(); 
+        const input = l3.value
+        if (input.length == 2) {
+            let cont = 0;
+         if (input === "mi" && cont !=1) {
+             l3.className = "input border border-success fs-2 rounded-1"    
+             ganados++;
+             cont=1; 
+             console.log(ganados)
+            }
+           
+        }
+     });
 
-}
+     let cont4 = 0;
+     
+     l4.addEventListener('keyup',(ev)=>{
+        ev.preventDefault(); 
+        const input = l4.value
+        if (input.length == 6) {
+            
+         if (input == "novia?" && cont4 !=1 ) {
+             l4.className = "input border border-success fs-2 rounded-1"    
+             ganados++;
+             cont4=1; 
+             console.log(ganados);
+             if (ganados >= 5) {
+                localStorage.clear();
+            }
+            }
+           
+        }
+     });
+    
 
 letras();
