046fe9a7ef5e1c3b574d35beb3a69ee81cddb4db
Kevin Soto <kevinalexandersoto999@gmail.com>
2021-11-23 12:08:02 -0500

    prueba de piroctenia

--- js/palabra.js
+++ js/palabra.js
@@ -31,7 +31,7 @@ const letras = ()=>{
        if (l1.value.length == 7) {
         let cont = 0;
         if (l1.value == "Quieres" && cont !=1) {
-            l1.className = "input border border-success fs-2 rounded-1"; 
+            l1.className = "input border border-success fs-2 border-3 rounded-1"; 
             ganados++;
             cont=1;   
         }
@@ -44,23 +44,23 @@ const letras = ()=>{
         if (l2.value.length == 3) {
             let cont = 0;
          if (l2.value == "ser" && cont !=1) {
-             l2.className = "input border border-success fs-2 rounded-1"    
+             l2.className = "input border border-success fs-2 border-3 rounded-1"    
              ganados++;
              cont=1; 
             }
            
         }
      });
-
+     let cont3 = 0;
      l3.addEventListener('keyup',(ev)=>{
         ev.preventDefault(); 
         const input = l3.value
         if (input.length == 2) {
-            let cont = 0;
-         if (input === "mi" && cont !=1) {
-             l3.className = "input border border-success fs-2 rounded-1"    
+            
+         if (input === "mi" && cont3 !=1) {
+             l3.className = "input border border-success fs-2 border-3 rounded-1"    
              ganados++;
-             cont=1; 
+             cont3=1; 
              console.log(ganados)
             }
            
@@ -75,13 +75,19 @@ const letras = ()=>{
         if (input.length == 6) {
             
          if (input == "novia?" && cont4 !=1 ) {
-             l4.className = "input border border-success fs-2 rounded-1"    
+             l4.className = "input border border-success fs-2 border-3 rounded-1"    
              ganados++;
              cont4=1; 
+
              console.log(ganados);
-             if (ganados >= 5) {
+
+             if (ganados >= 4) {
                 localStorage.clear();
+                start();
+
+	update();
             }
+
             }
            
         }
@@ -89,3 +95,121 @@ const letras = ()=>{
     
 
 letras();
+
+///////////pirogtenia//////////////
+
+var gravedad = .5;
+var numHijos = 10;
+
+var numParticulas = 20;
+var particulasCreadas = 0;
+
+function crearParticula() {
+	var particula = document.createElement("div");
+	particula.className="particula col-sm-12";
+
+	var y = window.innerHeight;
+	var x = Math.random() * window.innerWidth;
+
+	particula.style.top = y + "px";
+	particula.style.left = x + "px";
+
+	var velocidadY = -15 - (Math.random() * 15);
+
+	particula.setAttribute("data-velocidad-y", velocidadY);
+	particula.setAttribute("data-velocidad-x", "0");
+	particula.setAttribute("data-padre", "true");
+
+	particula.style.background = getRandomColor();
+
+	document.getElementsByTagName("body")[0].append(particula);
+
+	particulasCreadas++;
+
+	if (particulasCreadas < numParticulas) {
+	    setTimeout(crearParticula, 50 + (Math.random() * 150));
+	}
+}
+
+function start() {
+	crearParticula();
+}
+
+function update() {
+	var particulas = document.getElementsByClassName("particula");
+	for (var p=0; p < particulas.length; p++) {
+		var particula = particulas[p];
+
+		var velocidadY = parseFloat(particula.getAttribute("data-velocidad-y"));
+		velocidadY += gravedad;
+
+		particula.setAttribute("data-velocidad-y", velocidadY);
+
+		var top = particula.style.top ? particula.style.top : "0"; //10px
+		top = parseFloat(top.replace("px", ""));
+		top += velocidadY;
+		particula.style.top = top + "px";
+
+		var velocidadX = parseFloat(particula.getAttribute("data-velocidad-x"));
+
+		var left = particula.style.left ? particula.style.left : "0";
+		left = parseFloat(left.replace("px", ""));
+		left += velocidadX;
+		particula.style.left = left + "px";
+
+		var padre = particula.getAttribute("data-padre");
+
+		if (velocidadY >= 0 && padre === "true") {
+			explotar(particula);
+		}
+
+		if (top > window.innerHeight) {
+			particula.remove();
+		}
+	}
+
+	setTimeout(update, 20);
+}
+
+function explotar(particula) {
+
+	for (var h=0; h < numHijos; h++) {
+		var hijo = document.createElement("div");
+		hijo.className = "particula";
+
+		hijo.style.top = particula.style.top;
+		hijo.style.left = particula.style.left;
+		hijo.style.background = particula.style.background;
+
+		var velocidadY = (Math.random() * 20) - 18;
+		hijo.setAttribute("data-velocidad-y", velocidadY);
+		var velocidadX = (Math.random() * 16) - 8;
+		hijo.setAttribute("data-velocidad-x", velocidadX);
+
+
+		hijo.setAttribute("data-padre", false);
+
+		//Agregar el hijo :) :) :)
+		document.getElementsByTagName("body")[0].append(hijo);
+	}
+
+    particula.remove();
+}
+
+
+/* window.onload = function() {
+	start();
+
+	update();
+};
+ */
+
+//utilerias
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
